5f7c52565017f37c4815fbe536f82a817ce6ebbc
Harish <harish@better.club>
2022-09-24 09:03:42 +0530

    section 10 - implementing multi-tier navigation

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,16 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./App";
 
 // Mount function to start up the app
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+  ReactDOM.render(<App history={history} />, el);
+  return {
+    onParentNavigate: ({ pathname: nextPathname }) => {
+      const { pathname } = history.location;
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
 };
 
 //If we're in development and isolation, call mount immediately
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.getElementById("_marketing-dev-root");
-  devRoot && mount(devRoot);
+  devRoot && mount(devRoot, { defaultHistory: createBrowserHistory() });
 }
 
 //We're running through container and we should export the mount function
